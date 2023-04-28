be852d1218b3dbf38a1bd768ecca40963e806736
Kenneth <nitramnek@gmail.com>
2023-04-28 22:58:35 +0300

    revert to old patient sudmit code

--- src/index.js
+++ src/index.js
@@ -1,138 +1,64 @@
-const apiUrl = 'http://localhost:3000';
+//k3n5c0d3
 const signInForm = document.querySelector('.signIn');
 const dashboard = document.querySelector('#dashboard');
 const doctorNameSpan = document.querySelector('#doctorName');
 const patientsDeu = document.querySelector('#patientsDeu');
-const patientsTreated = document.querySelector('#patientsTreated');
-const patientDetails = document.querySelector('#patientDetails');
-const usernameInput = document.getElementById('username');
-const passwordInput = document.getElementById('password');
-
-
-async function signInDoctor(username, password) {
-  const response = await fetch(`${apiUrl}/doctors`);
-  const doctors = await response.json();
-  return doctors.find(d => d.username === username && d.password === password);
-}
-
-async function getPatients() {
-  const response = await fetch(`${apiUrl}/patients`);
-  return await response.json();
-}
-
-async function updatePatient(id, updatedData) {
-  const response = await fetch(`${apiUrl}/patients/${id}`, {
-    method: 'PUT',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(updatedData)
-  });
-  return await response.json();
-}
+const patientsTreaded = document.querySelector('#patientsTreated');
 
 function displayDashboard(doctor) {
   doctorNameSpan.textContent = doctor.displayname;
+  patientsDeu.innerHTML = '';
+  patientsTreaded.innerHTML = '';
+  fetch('http://localhost:3000/patients')
+    .then(response => response.json())
+    .then(patients => {
+      patients.forEach(patient => {
+        if (!patient.treated) {
+          const li = document.createElement('li');
+          li.textContent = patient.firstName + ' ' + patient.lastName;
+          patientsDeu.appendChild(li);
+        } else {
+          const li = document.createElement('li');
+          li.textContent = patient.firstName + ' ' + patient.lastName;
+          patientsTreaded.appendChild(li);
+        }
+      });
+    })
+    .catch(error => {
+      console.error(error);
+      const message = document.createElement('div');
+      message.textContent = 'An error occurred. Please try again later.';
+      message.classList.add('message', 'error');
+      signInForm.appendChild(message);
+    });
   signInForm.style.display = 'none';
   dashboard.style.display = 'block';
 }
 
-function displayPatients(patients) {
-  patientsDeu.innerHTML = '';
-  patientsTreated.innerHTML = '';
-  patients.forEach(patient => {
-    const li = document.createElement('li');
-    li.textContent = patient.firstName + ' ' + patient.lastName;
-    li.addEventListener('click', () => displayPatientDetails(patient));
-    li.style.cursor = 'pointer';
-    if (!patient.treated) {
-      patientsDeu.appendChild(li);
-    } else {
-      patientsTreated.appendChild(li);
-    }
-  });
-}
-
-function displayPatientDetails(patient) {
-  const details = `
-    <ul>
-      <li>Name: ${patient.firstName} ${patient.lastName}</li>
-      <li>Gender: ${patient.gender}</li>
-      <li>Age: ${patient.age}</li>
-      <li>Temperature: ${patient.temp}C</li>
-      <li>Blood Pressure: ${patient.BP}</li>
-      <li>Illness: ${patient.elment}</li>
-      <li>Treated: ${patient.treated ? 'Yes' : 'No'}</li>
-    </ul>
-    <form id="updateSymptoms">
-      <h3>Update Patient Symptoms:</h3>
-      <label>
-        Temperature:
-        <input type="number" name="temp" value="${patient.temp}">
-      </label>
-      <label>
-        Blood Pressure:
-        <input type="text" name="BP" value="${patient.BP}">
-      </label>
-      <label>
-        Illness:
-        <input type="text" name="elment" value="${patient.elment}">
-      </label>
-      <button type="submit">Update</button>
-    </form>
-  `;
-  patientDetails.innerHTML = details;
-  const updateForm = patientDetails.querySelector('#updateSymptoms');
-  updateForm.addEventListener('submit', async event => {
-    event.preventDefault();
-    const formData = new FormData(updateForm);
-    const updatedPatient = {
-      temp: formData.get('temp'),
-      BP: formData.get('BP'),
-      elment: formData.get('elment'),
-      treated: true
-    };
-    try {
-      const patient = await updatePatient(patient.id, updatedPatient);
-      displayPatients(await getPatients());
-      displayPatientDetails(patient);
-    } catch (error) {
+signInForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const username = event.target.username.value;
+  const password = event.target.password.value;
+  fetch('http://localhost:3000/doctors')
+    .then(response => response.json())
+    .then(doctors => {
+      const doctor = doctors.find(d => d.username === username && d.password === password);
+      if (doctor) {
+        displayDashboard(doctor);
+      } else {
+        const message = document.createElement('div');
+        message.textContent = 'Invalid username or password.';
+        message.classList.add('message', 'error');
+        signInForm.appendChild(message);
+      }
+    })
+    .catch(error => {
       console.error(error);
       const message = document.createElement('div');
-      message.textContent = error.message;
+      message.textContent = 'An error occurred. Please try again later.';
       message.classList.add('message', 'error');
-      patientDetails.appendChild(message);
-    }
-  });
-}
-if (!signInForm || !usernameInput || !passwordInput) {
-  console.error('One or more form elements not found');
-}
-
-signInForm.addEventListener('submit', async event => {
-  event.preventDefault();
-  const username = usernameInput.value;
-  const password = passwordInput.value;
-
-  try {
-    const response = await fetch('/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ username, password })
+      signInForm.appendChild(message);
     });
-    const data = await response.json();
-    if (response.ok) {
-      // Login was successful
-      window.location.href = '/dashboard';
-    } else {
-      // Login failed
-      const error = new Error(data.message || 'Unable to sign in');
-      throw error;
-    }
-  } catch (error) {
-    console.error(error);
-    alert(error.message);
-  }
 });
 
 
